73ee7fef8ec6b6ce5b09efaab0c3e54f4586a152
Santiago Ramos <35112126+santironhacker@users.noreply.github.com>
2024-12-11 11:19:38 +0700

    [main] Added user-item extra user details. Displayed more information based on user click and reset factors.

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -1,26 +1,54 @@
-import { Component, input } from '@angular/core';
-import { User } from '../../models/user.model'
+import {
+  Component,
+  inject,
+  input,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { combineLatestWith, map } from 'rxjs';
+import { User } from '../../models/user.model';
+import { EventsService } from '../../services/events.service';
 
 @Component({
   selector: 'app-user-item',
   standalone: true,
   templateUrl: './user-item.component.html',
-  styleUrl: './user-item.component.scss'
+  styleUrl: './user-item.component.scss',
 })
 export class UserItemComponent {
-  user = input.required<User>()
-  allUsers = input.required<User[]>()
+  user = input.required<User>();
+  allUsers = input.required<User[]>();
+
+  eventsService = inject(EventsService);
+  userClickStatus: WritableSignal<boolean> = signal<boolean>(false);
+
+  isDisplayUserDetails = toSignal<boolean, boolean>(
+    this.eventsService.groupButtonClicked$.pipe(
+      combineLatestWith(toObservable(this.userClickStatus)),
+      map(
+        ([buttonClicked, userClickStatus]) => buttonClicked || userClickStatus,
+      ),
+    ),
+    {
+      initialValue: false,
+    },
+  );
+
+  handleClickUser(): void {
+    this.userClickStatus.update((status) => !status);
+  }
 
   /**
    * Get the count of users with same nationality
    */
   get nationalitiesCount(): number {
     if (!this.allUsers().length) {
-      return 0
+      return 0;
     }
 
     return this.allUsers().reduce((acc, user) => {
-      return user.nat === this.user().nat ? acc + 1 : acc
-    }, 0)
+      return user.nat === this.user().nat ? acc + 1 : acc;
+    }, 0);
   }
 }
